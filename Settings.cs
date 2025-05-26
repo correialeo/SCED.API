8ab3b9888670adb7fbbdaa1a859691dd378a856e
correialeo <leandroo774@outlook.com>
2025-05-26 11:43:27 -0300

    Adicionar arquivos de projeto.

---
+++ Settings.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
+
+namespace SCED.API
+{
+    public static class Settings
+    {
+        public static readonly string Secret = "qz6q${a);#rhdm{be,{n)$~}7};!a9x|";
+        public static IConfiguration _configuration { get; private set; }
+
+        public static void Initialize(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public static string GetEnvVars(string env)
+        {
+            return _configuration.GetValue<string>(env);
+        }
+
+        public static string encodePassword(string password)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(Secret);
+
+            string hashed = Convert.ToBase64String(KeyDerivation
+                .Pbkdf2(
+                    password: password,
+                    salt: bytes,
+                    prf: KeyDerivationPrf.HMACSHA256,
+                    iterationCount: 100000,
+                    numBytesRequested: 256 / 8
+                )
+            );
+
+            return hashed;
+        }
+
+        public static string GetConnectionString(int filial = 0)
+        {
+            string databaseServer = Environment.GetEnvironmentVariable("DATABASE__SERVER");
+            string databaseUser = Environment.GetEnvironmentVariable("DATABASE__USER");
+            string databasePass = Environment.GetEnvironmentVariable("DATABASE__PASSWORD");
+
+            string databaseName, databasePort;
+
+            if (filial == 1)
+            {
+                databaseName = Environment.GetEnvironmentVariable("DATABASE__NAME_SP");
+                databasePort = Environment.GetEnvironmentVariable("DATABASE__PORT_SP");
+            }
+            else
+            {
+                databaseName = Environment.GetEnvironmentVariable("DATABASE__NAME");
+                databasePort = Environment.GetEnvironmentVariable("DATABASE__PORT");
+            }
+
+            string connectionString = _configuration.GetConnectionString("mysql");
+            connectionString = string.Format(connectionString, databaseServer, databasePort, databaseName, databaseUser, databasePass);
+
+            return connectionString;
+        }
+    }
+}
