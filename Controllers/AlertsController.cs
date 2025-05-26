4b82dee2bbc7bfb14abd67bc87ed290a93c6506f
correialeo <leandroo774@outlook.com>
2025-05-26 13:15:06 -0300

    feat: controllers gerados com scaffolding para teste

---
+++ Controllers/AlertsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SCED.API.Domain.Entity;
+using SCED.API.Infrasctructure.Context;
+
+namespace SCED.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlertsController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public AlertsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Alerts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Alert>>> GetAlerts()
+        {
+            return await _context.Alerts.ToListAsync();
+        }
+
+        // GET: api/Alerts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Alert>> GetAlert(long id)
+        {
+            var alert = await _context.Alerts.FindAsync(id);
+
+            if (alert == null)
+            {
+                return NotFound();
+            }
+
+            return alert;
+        }
+
+        // PUT: api/Alerts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAlert(long id, Alert alert)
+        {
+            if (id != alert.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(alert).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlertExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Alerts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Alert>> PostAlert(Alert alert)
+        {
+            _context.Alerts.Add(alert);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAlert", new { id = alert.Id }, alert);
+        }
+
+        // DELETE: api/Alerts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAlert(long id)
+        {
+            var alert = await _context.Alerts.FindAsync(id);
+            if (alert == null)
+            {
+                return NotFound();
+            }
+
+            _context.Alerts.Remove(alert);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AlertExists(long id)
+        {
+            return _context.Alerts.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/ResourcesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SCED.API.Domain.Entity;
+using SCED.API.Infrasctructure.Context;
+
+namespace SCED.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourcesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public ResourcesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Resources
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Resource>>> GetResources()
+        {
+            return await _context.Resources.ToListAsync();
+        }
+
+        // GET: api/Resources/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Resource>> GetResource(long id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return resource;
+        }
+
+        // PUT: api/Resources/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutResource(long id, Resource resource)
+        {
+            if (id != resource.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(resource).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ResourceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Resources
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Resource>> PostResource(Resource resource)
+        {
+            _context.Resources.Add(resource);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetResource", new { id = resource.Id }, resource);
+        }
+
+        // DELETE: api/Resources/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteResource(long id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            _context.Resources.Remove(resource);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ResourceExists(long id)
+        {
+            return _context.Resources.Any(e => e.Id == id);
+        }
+    }
+}
