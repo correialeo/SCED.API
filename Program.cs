fc8c3f33e85006c23c3247f0708dc67c78ac0497
correialeo <leandroo774@outlook.com>
2025-05-28 17:26:51 -0300

    docs: add swagger doc

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using System.Reflection;
 using System.Text.Json.Serialization;
 using dotenv.net;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using SCED.API;
+using SCED.API.Domain.Interfaces;
 using SCED.API.Infrasctructure.Context;
+using SCED.API.Infrastructure.Repositories;
 using SCED.API.Interfaces;
 using SCED.API.Services;
 
@@ -38,11 +42,33 @@ services.AddControllers()
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
 
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API Disaster Shield",
+        Description = "O Swagger documenta os endpoints da API Disaster Shield.",
+        Contact = new OpenApiContact() { Name = "Leandro Correia", Email = "rm556203@fiap.com.br" }
+    });
+
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    x.IncludeXmlComments(xmlPath);
+});
+
 services.AddScoped<DeviceDataService>();
 builder.Services.AddScoped<IResourceService, ResourceService>();
 builder.Services.AddScoped<IShelterService, ShelterService>();
 builder.Services.AddScoped<IDeviceService, DeviceService>();
 builder.Services.AddScoped<IAlertService, AlertService>();
+
+services.AddScoped<IUnitOfWork, UnitOfWork>();
+services.AddScoped<IAlertRepository, AlertRepository>();
+services.AddScoped<IDeviceRepository, DeviceRepository>();
+services.AddScoped<IShelterRepository, ShelterRepository>();
+services.AddScoped<IResourceRepository, ResourceRepository>();
+services.AddScoped<IDeviceDataRepository, DeviceDataRepository>();
 
 var app = builder.Build();
 
